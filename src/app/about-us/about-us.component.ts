0340790d87464f457a86404f454b06b3e100c788
Oleg <olegozibko@gmail.com>
2018-05-07 13:11:46 +0300

    home task

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-us.component.css']
 })
 export class AboutUsComponent implements OnInit {
-
+  str1: String = '';
   constructor() { }
 
   ngOnInit() {
+    this.func1();
+  }
+  func1 () {
+    for (let i = 0; i < 100; i++) {
+      if (i % 3 === 0) {
+        this.str1 += i + 'Hello';
+      }
+      if (i % 5 === 0 && i % 3 !== 0) {
+        this.str1 += i + 'Word';
+      }
+      if (i % 5 === 0 && i % 3 === 0) {
+        this.str1 += i + 'HelloWord';
+      }
+    }
+    console.log(this.str1);
   }
 
 }
